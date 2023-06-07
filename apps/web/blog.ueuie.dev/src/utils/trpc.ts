b80068f25bd24e8a56f4f716de1bc25acef231ab
uceumice <hi@uceumice.com>
2023-06-07 19:21:49 +0200

    changed type name, changed production endpoint

--- apps/web/blog.ueuie.dev/src/utils/trpc.ts
+++ apps/web/blog.ueuie.dev/src/utils/trpc.ts
@@ -1,10 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import type { AppRouter } from '@uceumice/blog.ueuie.dev-apis';
+import type { Router } from '@uceumice/blog.ueuie.dev-apis';
 
-export const trpc = createTRPCProxyClient<AppRouter>({
+export const trpc = createTRPCProxyClient<Router>({
   links: [
     httpBatchLink({
-      url: (import.meta.env.DEV ? 'http://127.0.0.1:8787' : import.meta.env.SITE) + '/api/trpc',
+      url: (import.meta.env.DEV ? 'http://127.0.0.1:8787' : "https://blog.ueuie.workers.dev") + '/api/trpc',
     }),
   ],
 });
