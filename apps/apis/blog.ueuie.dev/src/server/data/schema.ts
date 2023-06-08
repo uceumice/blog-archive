245def747d260682fb2a398c53008b264394b8d0
uceumice <hi@uceumice.com>
2023-06-08 22:14:19 +0200

    fix: unused dendency imports

--- apps/apis/blog.ueuie.dev/src/server/data/schema.ts
+++ apps/apis/blog.ueuie.dev/src/server/data/schema.ts
@@ -1,4 +1,4 @@
-import { relations, sql } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
 import { sqliteTable, text, integer, sqliteTableCreator, uniqueIndex } from 'drizzle-orm/sqlite-core';
 // ----
 
