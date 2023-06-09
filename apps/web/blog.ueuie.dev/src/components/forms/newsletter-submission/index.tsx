e6efcc2694be903b459b28a9b6605c470db71ae4
uceumice <hi@uceumice.com>
2023-06-09 08:25:48 +0200

    fix: halt unfunctional notification subscription

--- apps/web/blog.ueuie.dev/src/components/forms/newsletter-submission/index.tsx
+++ apps/web/blog.ueuie.dev/src/components/forms/newsletter-submission/index.tsx
@@ -13,10 +13,11 @@ export function NewsletterSubscription() {
   });
 
   const onSubmit: SubmitHandler<NewsletterSubscription> = ({ email }) => {
-    trpc.newsletter.add.mutate({ email }).then((value) => {
-      console.log({ value });
-      localStorage.setItem('autofill.newsletter.email', email);
-    });
+    // TODO: Complete the email notifications functionality...
+    // trpc.newsletter.add.mutate({ email, lang: 'en', pref: { mode: 'light' } }).then((value) => {
+    //   console.log({ value });
+    //   localStorage.setItem('autofill.newsletter.email', email);
+    // });
   };
 
   return (
@@ -43,29 +44,6 @@ export function NewsletterSubscription() {
 
         <button class="btn btn-sm btn-square">{form.submitted ? <BellFill class="w-5 h-5" /> : <Bell class="w-5 h-5" />}</button>
       </Form>
-      {/* {form.response.data && (
-        <div
-          class={clsx([
-            "w-full",
-            "px-4 py-2",
-            "flex justify-between items-center",
-            "border border-primary",
-            // Mobile
-            "sm:backdrop-blur-lg max-sm:bg-base-100",
-          ])}
-        >
-          <p>You will be notified when the page goes live :)</p>
-          <form method="post">
-            <input hidden name="intent" value="del" />
-            <button
-              type="submit"
-              class={clsx(["btn btn-sm btn-circle btn-ghost"])}
-            >
-              <Cross class="w-5 h-5" />
-            </button>
-          </form>
-        </div>
-      )} */}
     </div>
   );
 }
